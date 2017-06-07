b90436c15e1825b108948a5bf5af266beb65596c
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2017-06-07 18:35:21 +0200

    separate concerns

--- tests/testthat/test_boxjson.R
+++ tests/testthat/test_boxjson.R
@@ -2,21 +2,21 @@
 
 testthat::context('box box atoms')
 
-testthat::test_that('detects atoms appropriately', {
+# setup
+unboxd <- jsonlite::toJSON(list(ac=11L, ab=22L, c=c(3L, 6L)), auto_unbox=TRUE)
+inboxd <- jsonlite::toJSON(list(a=1L, b=2L, c=c(3L, 6L)), auto_unbox=FALSE)
+noboxd <- jsonlite::toJSON(list(a="dumb string", 
+                                b=list(c=9999L, d=list(list(11L)))),
+                           auto_unbox=TRUE)
+pkgjson <- paste0("{\"name\":\"sheater\",\"version\":\"1.0.0\",", 
+                  "\"description\":\"\",\"main\":\"index.js\",",
+                  "\"scripts\":{\"test\":\"echo\\\"Error:no test specified\\\"&&exit1\"},",
+                  "\"keywords\":[],\"author\":\"\",\"license\":\"ISC\",",
+                  "\"dependencies\":{\"express\":\"^4.15.3\"}}")
+
+testthat::test_that('predicate function', {
   
-  # setup
-  unboxd <- jsonlite::toJSON(list(ac=11L, ab=22L, c=c(3L, 6L)), auto_unbox=TRUE)
-  inboxd <- jsonlite::toJSON(list(a=1L, b=2L, c=c(3L, 6L)), auto_unbox=FALSE)
-  noboxd <- jsonlite::toJSON(list(a="dumb string", 
-                                  b=list(c=9999L, d=list(list(11L)))),
-                             auto_unbox=TRUE)
-  pkgjson <- paste0("{\"name\":\"sheater\",\"version\":\"1.0.0\",", 
-                     "\"description\":\"\",\"main\":\"index.js\",",
-                     "\"scripts\":{\"test\":\"echo\\\"Error:no test specified\\\"&&exit1\"},",
-                     "\"keywords\":[],\"author\":\"\",\"license\":\"ISC\",",
-                     "\"dependencies\":{\"express\":\"^4.15.3\"}}")
-  
-  # predicate function
+  # predicate function bulk
   testthat::expect_identical(hasUnboxedAtom(unboxd), TRUE)
   testthat::expect_identical(hasUnboxedAtom(inboxd), FALSE)
   testthat::expect_identical(hasUnboxedAtom(noboxd), TRUE)
@@ -24,12 +24,16 @@ testthat::test_that('detects atoms appropriately', {
   testthat::expect_identical(hasUnboxedAtom(boxAtoms(pkgjson)), FALSE)
   testthat::expect_identical(hasUnboxedAtom('[[551],[66]]'), FALSE)
   testthat::expect_identical(hasUnboxedAtom('[[551],66]'), TRUE)
+
+})
+
+testthat::test_that('boxing', {
   
   # boxing pt 1
   testthat::expect_identical(boxAtoms(noboxd), 
-                               structure(paste0('{"a":["dumb string"],', 
-                                                '"b":{"c":[9999],"d":[[11]]}}'), 
-                                         class='json'))
+                             structure(paste0('{"a":["dumb string"],', 
+                                              '"b":{"c":[9999],"d":[[11]]}}'), 
+                                       class='json'))
   
   # boxing pt 2
   testthat::expect_identical(boxAtoms(unboxd),
@@ -65,6 +69,10 @@ testthat::test_that('detects atoms appropriately', {
   # boxing pt 8
   testthat::expect_identical(boxAtoms('[4,1,9]'),
                              structure('[4,1,9]', class='json'))
+  
+})
+
+testthat::test_that('unboxing', {
   
   # unboxing pt 1
   testthat::expect_identical(unboxAtoms(inboxd),
